34754e79d52d34c01abf868e6c5213246154ea8d
Claym64 <130917945+Claym64@users.noreply.github.com>
2023-10-30 03:31:41 +0500

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,27 +1,23 @@
-import { logger } from "@vendetta";
+import { logger, sendMessage } from "@vendetta";
 import Settings from "./Settings";
 
 export default {
     onLoad: () => {
-        logger.log("Плагин активирован!");
-        // Добавьте слушателя события "message" для обработки входящих сообщений.
-        vendetta.ui.on("message", handleMessage);
+        logger.log("Plugin loaded.");
     },
     onUnload: () => {
-        logger.log("Плагин деактивирован.");
-        // Удаляем слушателя события "message" при выгрузке плагина, чтобы избежать утечек памяти.
-        vendetta.ui.off("message", handleMessage);
+        logger.log("Plugin unloaded.");
     },
     settings: Settings,
-}
-
-// Функция для обработки входящих сообщений
-function handleMessage(message: string) {
-    // Проверяем, содержит ли сообщение слово "ок" (без учета регистра).
-    if (message.toLowerCase().includes("ок")) {
-        // Заменяем "ок" на "Окей".
-        const correctedMessage = message.replace(/ок/gi, "Окей");
-        // Отправляем исправленное сообщение.
-        vendetta.ui.sendMessage(correctedMessage);
-    }
+    events: {
+        onMessage: (message) => {
+            const content = message.content;
+            const replacedContent = content.replace(/\bок\b/gi, "Окей");
+            
+            if (replacedContent !== content) {
+                // Заменить только если слово "ок" найдено и заменено
+                sendMessage(message.channelId, replacedContent);
+            }
+        },
+    },
 }
