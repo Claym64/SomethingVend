e97a0657690547f2cc40803229c2dcd9e43478c1
Claym64 <130917945+Claym64@users.noreply.github.com>
2023-10-30 03:28:26 +0500

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -3,10 +3,25 @@ import Settings from "./Settings";
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        logger.log("Плагин активирован!");
+        // Добавьте слушателя события "message" для обработки входящих сообщений.
+        vendetta.ui.on("message", handleMessage);
     },
     onUnload: () => {
-        logger.log("Goodbye, world.");
+        logger.log("Плагин деактивирован.");
+        // Удаляем слушателя события "message" при выгрузке плагина, чтобы избежать утечек памяти.
+        vendetta.ui.off("message", handleMessage);
     },
     settings: Settings,
+}
+
+// Функция для обработки входящих сообщений
+function handleMessage(message: string) {
+    // Проверяем, содержит ли сообщение слово "ок" (без учета регистра).
+    if (message.toLowerCase().includes("ок")) {
+        // Заменяем "ок" на "Окей".
+        const correctedMessage = message.replace(/ок/gi, "Окей");
+        // Отправляем исправленное сообщение.
+        vendetta.ui.sendMessage(correctedMessage);
+    }
 }
